fb0ba6027cc227ece9b07c64c680d615ac252ab0
mgaldino <mgaldino@student.42sp.org.br>
2023-02-19 14:03:15 +0100

    cpp01 ex04 done

---
+++ module01/ex04/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int	main( void )
+{
+	std::string		file_name = "infile";
+	std::ifstream	ifs(file_name.c_str(), std::ios::in);
+	std::ofstream	ofs;
+	std::string		s;
+	std::string		s2 = "Sheldon";
+	std::string		s3 = "Moacir";
+	std::size_t		pos = 0;
+
+	if (ifs.is_open())
+	{
+		ofs.open((file_name + ".replace").c_str(), std::ios::out | std::ios::trunc);
+		while (std::getline(ifs, s))
+		{
+			pos = s.find(s2);
+			if (pos != std::string::npos)
+			{
+				s.erase(pos, s2.size());
+				s.insert(pos, s3);
+			}
+			ofs << s << std::endl;
+		}
+	}
+	else
+		std::cout << "Unable to open input file" << std::endl;
+	ifs.close();
+	ofs.close();
+	return 0;
+}
