aef20a1fbffffed055983832c0c5a870c9ec9430
mgaldino <mgaldino@student.42sp.org.br>
2023-02-23 19:51:06 +0100

    cpp02 ex00 done

---
+++ module02/ex00/Fixed.hpp
@@ -0,0 +1,14 @@
+class	Fixed
+{
+	private:
+		int					_value;
+		static const int	_fracBits = 8;
+	
+	public:
+		Fixed( void );
+		Fixed( const Fixed & src );
+		Fixed & operator=(const Fixed & rhs);
+		~Fixed( void );
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
+};
