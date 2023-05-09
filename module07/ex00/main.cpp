7146db6f49f78b38c3346fa1b51c7a372e5ef42d
mgaldino <mgaldino@student.42sp.org.br>
2023-05-09 15:11:22 +0200

    cpp07 ex00 done

---
+++ module07/ex00/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "whatever.hpp"
+
+// int	main( void )
+// {
+// 	std::cout << "---------------------------------------------------" << std::endl;
+// 	int	x = 1;
+// 	int y = 2;
+// 	std::cout << "Before swap:" << std::endl;
+// 	std::cout << "x = " << x << std::endl;
+// 	std::cout << "y = " << y << std::endl;
+// 	swap(&x, &y);
+// 	std::cout << "After swap:" << std::endl;
+// 	std::cout << "x = " << x << std::endl;
+// 	std::cout << "y = " << y << std::endl;
+// 	std::cout << "---------------------------------------------------" << std::endl;
+// 	std::cout << "---------------------------------------------------" << std::endl;
+// 	std::string	x = 1;
+// 	int y = 2;
+// 	std::cout << "Before swap:" << std::endl;
+// 	std::cout << "x = " << x << std::endl;
+// 	std::cout << "y = " << y << std::endl;
+// 	swap(&x, &y);
+// 	std::cout << "After swap:" << std::endl;
+// 	std::cout << "x = " << x << std::endl;
+// 	std::cout << "y = " << y << std::endl;
+// 	std::cout << "---------------------------------------------------" << std::endl;
+// 	return 0;
+// }
+
+int main( void ) {
+int a = 2;
+int b = 3;
+::swap( a, b );
+std::cout << "a = " << a << ", b = " << b << std::endl;
+std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+std::string c = "chaine1";
+std::string d = "chaine2";
+::swap(c, d);
+std::cout << "c = " << c << ", d = " << d << std::endl;
+std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+return 0;
+}
---
+++ module07/ex00/whatever.hpp
@@ -0,0 +1,25 @@
+template <typename T>
+void	swap(T& a, T& b)
+{
+	T aux = a;
+	a = b;
+	b = aux;
+}
+
+template <typename T>
+T	min(T a, T b)
+{
+	if (a < b)
+		return a;
+	else
+		return b;
+}
+
+template <typename T>
+T	max(T a, T b)
+{
+	if (a > b)
+		return a;
+	else
+		return b;
+}
